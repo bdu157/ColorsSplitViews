ea26113594b7fa833415dfb201a740f4a1c63db5
Dongwoo Pae <dongwoopae@gmail.com>
2019-12-06 12:43:06 +0900

    first commit

---
+++ ColorsSplitViews/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  ColorsSplitViews
+//
+//  Created by Dongwoo Pae on 12/6/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    let name: String
+    let color: UIColor
+}
---
+++ ColorsSplitViews/ColorController.swift
@@ -0,0 +1,23 @@
+//
+//  ColorController.swift
+//  ColorsSplitViews
+//
+//  Created by Dongwoo Pae on 12/6/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColorContoller {
+    let colors = [
+        Color(name: "Red", color: .systemRed),
+        Color(name: "Orange", color: .systemOrange),
+        Color(name: "Blue", color: .systemBlue),
+        Color(name: "Yellow", color: .systemYellow),
+        Color(name: "Green", color: .systemGreen),
+        Color(name: "Grey", color: .systemGray),
+        Color(name: "Purple", color: .systemPurple),
+        Color(name: "Indigo", color: .systemIndigo)
+    ]
+}
